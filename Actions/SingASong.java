26fb8c1a3e13a53dfd03c55d93ea7eda29af60e7
Ellory <edoyle@smith.edu>
2023-05-04 12:12:50 -0400

    This is a Mess

---
+++ Actions/SingASong.java
@@ -0,0 +1,9 @@
+package Actions;
+public class SingASong {
+	
+	//Go to club
+	//pick a genre
+	//random generate a member to react
+	//desire goes up when singing
+	//genre determines club unity
+}
---
+++ Actions/TalkWithClique.java
@@ -0,0 +1,11 @@
+package Actions;
+
+public class TalkWithClique {
+
+	///go to area of clique (go to group class)
+	///random generate someone to interact with (not in glee)
+	///three options for each clique
+	///desire to win goes up if you are rude to clique member
+	///desire to win falls if you are nice to clique member
+	///club unity goes up if desire to win goes up
+}
